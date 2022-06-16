6c26d883387ba80e145ecf13dba52097beb6e4e7
Gabriel Longatti <GaLongatti@hotmail.com>
2022-06-16 14:50:14 -0300

    alteracao startup

--- Startup.cs
+++ Startup.cs
@@ -57,9 +57,7 @@ namespace Locadora
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapControllers();
 			});
 		}
 	}
