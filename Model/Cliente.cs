02b3f8c3e92a734a4c660e5fcf2da8a71127b203
Gabriel Longatti <GaLongatti@hotmail.com>
2022-06-15 22:51:37 -0300

    criação de classes conforme escopo

---
+++ Model/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace Locadora.Model
+{
+	public class Cliente
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Documento { get; set; }
+		public bool Ativo { get; set; }
+
+	}
+}
---
+++ Model/Locacao.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Locadora.Model
+{
+	public class Locacao
+	{
+		public int Id { get; set; }
+		public int IdCliente { get; set; }
+		public Cliente Cliente { get; set; }
+		public List<Filme> FilmesAlugados { get; set; }
+	}
+}
