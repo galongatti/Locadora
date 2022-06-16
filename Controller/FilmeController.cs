b9426fa211d5ae88fb571df1acfb5ed4b39dd0f9
Gabriel Longatti <GaLongatti@hotmail.com>
2022-06-16 15:07:47 -0300

    crud filme

---
+++ Controller/FilmeController.cs
@@ -0,0 +1,115 @@
+﻿using Locadora.Context;
+using Locadora.Logica;
+using Locadora.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Locadora.Controller
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class FilmeController : ControllerBase
+	{
+		private readonly LocadoraApiContext _context;
+
+		public FilmeController(LocadoraApiContext context)
+		{
+			_context = context;
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Filme>> CadastrarFilme(Filme Filme)
+		{
+			try
+			{
+				if (!ModelState.IsValid) { return BadRequest("Objeto filme inválido"); }
+
+				LogicaFilme logica = new LogicaFilme(_context);
+				List<string> erros = logica.ValidarDados(Filme);
+
+				if(erros.Count > 0)
+				{
+					return BadRequest(erros);
+				}
+				else
+				{
+					Filme newFilme = await logica.CadastrarFilme(Filme);
+					return Ok(newFilme);
+				}
+			}
+			catch (Exception)
+			{
+				return BadRequest("Erro ao cadastrar filme");
+			}
+		}
+
+		[HttpPut]
+		public async Task<ActionResult<Filme>> AtualizarFilme(Filme filme)
+		{
+			try
+			{
+				if (!ModelState.IsValid) { return BadRequest("Objeto filme inválido"); }
+
+				LogicaFilme logica = new LogicaFilme(_context);
+				List<string> erros = logica.ValidarDados(filme);
+
+				if (erros.Count > 0)
+				{
+					return BadRequest(erros);
+				}
+				else
+				{
+					Filme FilmeExiste = await logica.BuscarFilmeId(filme.Id);
+					if(FilmeExiste != null)
+					{
+						Filme newFilme = await logica.AtualizarFilme(filme);
+						return Ok(newFilme);
+					} 
+					else
+					{
+						return BadRequest("Filme não localizado");
+					}
+					
+				}
+			}
+			catch (Exception)
+			{
+				return BadRequest("Erro ao atualizar filme");
+			}
+		}
+
+		[HttpGet("BuscarTodosFilmes")]
+		public async Task<ActionResult<List<Filme>>> BuscarTodosFilmes()
+		{
+			try
+			{
+				LogicaFilme logica = new LogicaFilme(_context);
+				return await logica.BuscarTodosFilmes();
+			}
+			catch (Exception)
+			{
+
+				return BadRequest("Erro ao buscar Filmes");
+			}
+		}
+
+		[HttpGet("BuscarById/{id:int}")]
+		public async Task<ActionResult<Filme>> BuscarFilmePorId(int id)
+		{
+			try
+			{
+				LogicaFilme logica = new LogicaFilme(_context);
+				return await logica.BuscarFilmeId(id);
+			}
+			catch (Exception)
+			{
+
+				return BadRequest("Erro ao buscar Filmes");
+			}
+		}
+
+
+	}
+}
---
+++ Logica/LogicaFilme.cs
@@ -0,0 +1,56 @@
+﻿using Locadora.Context;
+using Locadora.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Locadora.Logica
+{
+	public class LogicaFilme
+	{
+		private readonly LocadoraApiContext _context;
+		private readonly DbSet<Filme> _dbSet;
+		public LogicaFilme(LocadoraApiContext context)
+		{
+			_context = context;
+			_dbSet = context.Filme;
+		}
+
+		public List<string> ValidarDados(Filme cliente)
+		{
+			List<string> listaErros = new List<string>();
+
+			if (string.IsNullOrEmpty(cliente.Nome))
+				listaErros.Add("O nome do filme deve ser preenchido");
+
+			return listaErros;
+		}
+
+		public async Task<Filme> CadastrarFilme(Filme filme)
+		{
+			_context.Filme.Add(filme);
+			await _context.SaveChangesAsync();
+			return filme;
+		}
+
+		public async Task<Filme> AtualizarFilme(Filme filme)
+		{
+			_context.Filme.Update(filme);
+			await _context.SaveChangesAsync();
+			return filme;
+		}
+
+		public async Task<Filme> BuscarFilmeId(int id)
+		{
+			Filme filme = await _dbSet.AsNoTracking().FirstOrDefaultAsync(x => x.Id.Equals(id));
+			return filme;
+		}
+
+		public async Task<List<Filme>> BuscarTodosFilmes()
+		{
+			List<Filme> filmes = await _dbSet.AsNoTracking().ToListAsync();
+			return filmes;
+		}
+
+	}
+}
